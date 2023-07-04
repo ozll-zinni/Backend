5c091f0201b9a648f83459d70b1a2af3522ba7c7
SIWON990327 <pos06058@naver.com>
2023-07-04 15:48:23 +0900

    초기

---
+++ src/test/java/com/example/traveler/TravelerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.traveler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
