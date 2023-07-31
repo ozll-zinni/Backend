1eef525dded506c72fe5e03c8e6d4bf39f850925
SIWON990327 <pos06058@naver.com>
2023-08-01 03:38:38 +0900

    Add UserController mytravels

--- src/main/java/com/example/traveler/controller/UserController.java
+++ src/main/java/com/example/traveler/controller/UserController.java
@@ -3,10 +3,12 @@ package com.example.traveler.controller;
 import com.example.traveler.config.BaseException;
 import com.example.traveler.config.BaseResponse;
 import com.example.traveler.jwt.JwtTokenProvider;
+import com.example.traveler.model.dto.TravelResponse;
 import com.example.traveler.model.dto.UpdateNicknameDTO;
 import com.example.traveler.model.entity.User;
 import com.example.traveler.repository.UserRepository;
 import com.example.traveler.service.S3Uploader;
+import com.example.traveler.service.TravelService;
 import com.example.traveler.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.example.traveler.config.BaseResponseStatus.INVALID_JWT;
@@ -31,12 +34,15 @@ public class UserController {
 
     private final S3Uploader s3Uploader;
 
+    private final TravelService travelService;
+
     @Autowired
-    public UserController(JwtTokenProvider jwtTokenProvider, UserService userService, UserRepository userRepository, S3Uploader s3Uploader) {
+    public UserController(JwtTokenProvider jwtTokenProvider, UserService userService, UserRepository userRepository, S3Uploader s3Uploader, TravelService travelService) {
         this.userService = userService;
         this.jwtTokenProvider = jwtTokenProvider;
         this.userRepository = userRepository;
         this.s3Uploader = s3Uploader;
+        this.travelService = travelService;
     }
 
     @PatchMapping("/nickname")
@@ -104,6 +110,26 @@ public class UserController {
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
+    }
+
+    @GetMapping("/my_travels")
+    public ResponseEntity<List<TravelResponse>> getAllMyTravel(@RequestHeader("Authorization") String accessToken) {
+            // 유저 정보를 통해 해당 사용자의 여행 정보를 가져옴
+            Long Id = jwtTokenProvider.extractId(accessToken);
+            User user = userService.getUser(Id); // User 객체를 생성하거나 UserRepository를 이용하여 사용자 정보를 가져올 수 있습니다.
+            List<TravelResponse> allMyTravelResponse = travelService.getAllMyTravel(user);
+            return ResponseEntity.ok(allMyTravelResponse);
+
+    }
+
+    @GetMapping("/my_past_travels")
+    public ResponseEntity<List<TravelResponse>> allMyPastTravel(@RequestHeader("Authorization") String accessToken) {
+        // 유저 정보를 통해 해당 사용자의 여행 정보를 가져옴
+        Long Id = jwtTokenProvider.extractId(accessToken);
+        User user = userService.getUser(Id); // User 객체를 생성하거나 UserRepository를 이용하여 사용자 정보를 가져올 수 있습니다.
+        List<TravelResponse> allMyTravelResponse = travelService.allMyPastTravel(user);
+        return ResponseEntity.ok(allMyTravelResponse);
+
     }
 }
 
