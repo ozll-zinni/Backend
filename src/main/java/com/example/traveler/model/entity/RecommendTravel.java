48e4f7ef76c9bd6957c0295739ffc88a5db1226f
SIWON990327 <pos06058@naver.com>
2023-08-11 22:59:37 +0900

    feat: fix RecommendTravel

--- src/main/java/com/example/traveler/model/entity/RecommendTravel.java
+++ src/main/java/com/example/traveler/model/entity/RecommendTravel.java
@@ -26,11 +26,9 @@ public class RecommendTravel {
     /***
 
      code1 - countryId, what, hard, with, period 순으로 조합
-     code2 - countryId, what, hard 순으로 조합
 
      what - 1. 경치관람, 2. 먹방, 3. 액티비티, 4. 체험, 5. 카페
      hard - 1. 빡빡하게, 2. 보통, 3. 느긋하게
-     with - 1. 친구와, 2. 가족과, 3. 연인과, 4. 혼자서
      period - 1박 2일이면 2 저장
 
      ***/
