63b9f8d15e97ccebbf6c241699fac8051050b6bc
현아 <52688527+hyeonahhh@users.noreply.github.com>
2023-08-20 00:09:55 +0900

    feat: fix cors error2

--- src/main/java/com/example/traveler/config/WebConfig.java
+++ src/main/java/com/example/traveler/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.example.traveler.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
